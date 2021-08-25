8a216e33cc455710dfd2fc6648e901c32146a545
Leevi Laitala <leevi.laitala@protonmail.com>
2021-08-25 21:28:01 +0300

    Rewrote the whole damn thing

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 Screenshot tool with dmenu ui written in shell
 
+
+
+## Features
+
+- Take screenshots (with shotgun)
+- Drag custom region with your mouse, or click on open windows to get it's dimensions (with slop)
+- Upload to imgur
+
+
+
 ### Depencies:
 - slop
 - shotgun
 - dunst
 - imgurqt
 - dmenu
-- zbar
 - xclip
--- screenshot.sh
+++ screenshot.sh
@@ -1,120 +1,55 @@
-#!/bin/sh
-snd="/usr/share/sounds/freedesktop/stereo/camera-shutter.oga"
-destDir="/home/$USER/Pictures/screenshots"
-fname="$destDir/$(date +'%Y-%m-%d-%T').png"
+#!/usr/bin/sh
+path="/home/$USER/Code/screenshot-tool/"
+tmp="/tmp/"
 
-qrDestDir="/home/$USER/Documents"
-qrfname="$qrDestDir/qr-scan-$(date +'%Y-%m-%d-%T')"
+iconError="report_problem"
+iconSuccess="add_photo_alternate"
+iconImgur="cloud_upload"
 
+# Notification functions
+errorMessage () {
+    # $1 error message string
+    dunstify -i $iconError -t 5000 -u critical "Taking screenshot failed" "$1"
+    [ -f "$path$file" ] && rm -f "$path$file"
 
-# Actions when screenshot was taken
-opt1="Save to Disk"
-opt2="Upload to Imgur"
-opt3="Scan for QR codes"
-opt4="Cancel"
-
-# Actions when QR code was scanned
-qropt1="Copy to clipboard"
-qropt2="Open in web browser"
-qropt3="Save to file"
-qropt4="Cancel"
-
-# Icons for dunstify
-icoError="report_problem"
-icoImgur="cloud_upload"
-icoQR="qrcode"
-icoImg="add_photo_alternate"
-
-if [[ ! -d $destDir ]]; then
-    mkdir -p $destDir
-fi
-
-if [[ ! -d $qrDestDir ]]; then
-    mkdir -p $qrDestDir
-fi
-
-area=$(slop -c 1,1,1,1)
-
-if [ $? -eq 0 ]; then
-   shotgun -g $area -f png "$fname"
-else
-   dunstify -i $icoError -t 5000 -u critical "Screenshot failed" "Taking screenshot cancelled by user"
-   exit 1
-fi
-
-paplay --volume=40000 $snd &
-
-if [[ -e $fname ]]; then
-  opt=$(echo -e "$opt1\n$opt2\n$opt3\n$opt4" | dmenu -p "Screenshot taken, what next?")
-
-  case $opt in
-  $opt1)
-    dunstify -i $icoImg -t 5000 "Screenshot taken" "Screenshot saved to $fname\n Size: $(du -h $fname | awk '{print $1}')"
-    exit 0
-    ;;
-  $opt2)
-    url=$(imgurqt --anonymous $fname | grep Image | awk '{print $3}')
-    size=$(du -h $fname | awk '{print $1}')
-    rm -f $fname
-    echo "$url" | xclip -selection clipboard
-    dunstify -i $icoImgur -t 5000 "Screenshot taken" "Screenshot uploaded to imgur with address $url\n \nURL copied to clipboard\n Size: $size"
-    exit 0
-    ;;
-  $opt3)
-    qr="$(zbarimg --oneshot -q $fname | sed 's/QR-Code://g')"
-
-    if ! [ -z $qr ]; then
-      input=$qr
-      limit=16
-
-      if [ ${#input} -gt $(($limit + 3)) ]; then
-        out=$(echo "${input:0:$limit}...")
-      else
-        out=$qr
-      fi
-
-      qropt=$(echo -e "$qropt1\n$qropt2\n$qropt3\n$qropt4" | dmenu -p "QR : $out")
-
-      case $qropt in
-      $qropt1)
-        echo -e "$qr" | xclip -selection clipboard
-        rm -f $fname
-        dunstify -i $icoQR -t 5000 "QR code was found" "QR code found in screenshot, contents copied to clipboard\nContents: $qr"
-        exit 0
-        ;;
-      $qropt2)
-        xdg-open $qr
-        rm -f $fname
-        dunstify -i $icoQR -t 5000 "QR code was found" "QR code found in screenshot, contents opened in web browser\nContents: $qr"
-        exit 0
-        ;;
-      $qropt3)
-        echo -e "$qr" >> $qrfname 
-        rm -f $fname
-        dunstify -i $icoQR -t 5000 "QR code was found" "QR code found in screenshot, contents saved to $qrfname\nContents: $qr"
-        exit 0
-        ;;
-      $qropt4)
-        rm -f $fname
-        dunstify -i $icoError -t 5000 -u critical "QR code scan failed" "Scanning QR code was cancelled by user"
-        exit 0
-        ;;
-      esac
-
-
-      rm -f $fname
-      dunstify -i $icoError -t 5000 -u critical "QR code scan failed" "Scanning QR code was cancelled by user"
-      exit 1
-    fi
-
-    rm -f $fname
-    dunstify -i $icoError -t 5000 -u critical "QR code scan failed" "No QR code was found in the image"
     exit 1
-    ;;
-  esac
+}
 
-  rm -f $fname
-  dunstify -i $icoError -t 5000 -u critical "Screenshot failed" "Taking screenshot cancelled by user"
-else
-  dunstify -i $icoError -t 5000 -u critical "Screenshot failed" "Saving or taking screenshot failed"
-fi
+notify () {
+    # $1 title, $2 message, $3 icon
+    dunstify -i "$3" -t 5000 -u low "$1" "$2"
+}
+
+geometry="$(slop -q -b 2)" || errorMessage "Cancelled by user"
+
+file="$(date +'%Y-%m-%d_%H-%M-%S').png"
+
+opt1="Keep screenshot"
+opt2="Upload to Imgur & Keep screenshot"
+opt3="Delete screenshot"
+
+# Take screenshot
+shotgun -f png -g $geometry "$path$file" && \
+    opt=$(echo -e "$opt1\n$opt2\n$opt3" | dmenu -i -p "What next?" | grep "\<$opt1\>\|\<$opt2\>\|\<$opt3\>")
+
+[ -z "$opt" ] && errorMessage "Cancelled by user"
+
+case $opt in
+    $opt1) # Keep screenshot
+        notify "Screenshot taken" "Saved to $path\nwith size of $(du -h $path$file | awk '{print $1}')" $iconSuccess
+        exit 0
+        ;;
+    $opt2) # Upload to imgur
+        imgurdata="$tmp$(date +'imgurupload-%N')"
+        imgurqt --anonymous "$path$file" >> $imgurdata && \
+            cat $imgurdata | awk '/Image/ {print $3}' | xclip -selection clipboard && \
+            notify "Uploaded to Imgur" "URL copied to clipboard, more details in\n$imgurdata" ‰iconImgur
+        exit 0
+        ;;
+    $opt3) # Delete screenshot
+        rm -f "$path$file" && notify "Screenshot deleted" "Screenshot deleted" $iconError
+        exit 0
+        ;;
+esac
+
+errorMessage ""
